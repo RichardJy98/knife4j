0277689f272afd53d908861938afa12f111df82e
Jay <1006570515@qq.com>
2022-07-03 14:15:41 +0800

    xv

--- src/main/java/com/richard/controller/IndexController.java
+++ src/main/java/com/richard/controller/IndexController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     public static final String TYPE = "测试";
+    
 
     @ApiImplicitParam(name = "name", value = "姓名", required = true)
     @ApiOperation(value = "向客人问好")
